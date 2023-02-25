98004425da5d6fa86f4f448174c7372e20ff4ab5
Roman Balakleec <lrem1x777@gmail.com>
2023-02-26 00:08:45 +0200

    fix modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,48 +1,14 @@
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
-import { CloseBtn, Image, ModalStyled, Overlay } from './Modal.styled';
-import { createPortal } from 'react-dom';
+import { Overlay, ModalImg } from './Modal.styled';
 
-const modalRoot = document.querySelector('#modal-root');
-
-export default class Modal extends PureComponent {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-      window.removeEventListener('keydown', this.handleKeyDown);
-    }
+export const Modal = ({ srcBig, alt, onClick }) => {
+  const hadleClick = e => {
+    e.stopPropagation();
   };
-
-  handleBackdropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    const { url, onClose, tags } = this.props;
-
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalStyled>
-          <Image src={url} alt={tags} />
-        </ModalStyled>
-        <CloseBtn type="button" onClick={onClose}>
-          Close
-        </CloseBtn>
-      </Overlay>,
-
-      modalRoot
-    );
-  }
-}
-
-Modal.propTypes = {
-  url: PropTypes.string.isRequired,
-  tags: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
+  return (
+    <Overlay onClick={onClick}>
+      <ModalImg onClick={hadleClick}>
+        <img src={srcBig} alt={alt} />
+      </ModalImg>
+    </Overlay>
+  );
 };
